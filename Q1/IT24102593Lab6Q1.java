d2888c077b4861f344d6e8ea83a61f130154352c
IT24102593 <it24102593@my.sliit.lk>
2024-08-28 10:04:14 +0530

    IT24102593 Induwara H.M.D

---
+++ Q1/IT24102593Lab6Q1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class IT24102593Lab6Q1 {
+
+    public static void main(String[] args) {
+
+        Scanner value = new Scanner(System.in);
+
+        System.out.print("Enter a number:");
+        int number = value.nextInt();
+
+        double squareOfNumber = number * number;
+        System.out.println("The square of" + number+" is : " + squareOfNumber);
+
+        double squareRootOfNumber = Math.sqrt(number);
+        System.out.println("The square root of " + number + " is : " + squareRootOfNumber);
+
+    }
+
+}
